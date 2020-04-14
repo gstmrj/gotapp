96128b294a9ed8242d12ecbf040a12a937dbedc1
Guilherme Mendes <gstmrj@icloud.com>
2020-04-14 22:11:19 +0200

    Primeira tela mostrando o RateViewController.xib

---
+++ gotapp/RateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RateViewController.swift
+//  gotapp
+//
+//  Created by Guilherme Mendes on 14/04/2020.
+//  Copyright © 2020 Guilherme Mendes. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- gotapp/ViewController.swift
+++ gotapp/ViewController.swift
@@ -6,15 +6,4 @@
 //  Copyright © 2020 Guilherme Mendes. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+import Foundation
